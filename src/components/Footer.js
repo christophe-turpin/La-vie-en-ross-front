51d8100ecf61385b8c713b15e5aa2f55c2ee3993
kphiri84 <kenny.phiri@laposte.net>
2020-10-28 18:09:39 +0100

    on work : footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,22 @@
 import React from 'react';
 import { Icon } from 'semantic-ui-react';
 import './footer.css';
-import chris from '../assets/images/logo_ct_rondnoir_blanc.png'
-import ken from '../assets/images/logoK.png'
+import chris from '../assets/images/logo_ct_rondnoir_blanc.png';
+import ken from '../assets/images/logoK.png';
 
 const Footer = () => {
 	return (
 		<div className="footer">
+			<h4 className="devItem">Ce site a été crée par :</h4>
 			<div className="devContent">
-				<h4 className="devItem">Ce site a été crée par :</h4>
-				<ul>
-					<li>
-						<h5 className="devItem">
-							Christophe :<a href="https://cv-christophe-turpin.netlify.app/" target="blank">
-								<img src={chris} className="logodev" alt="logo christophe" />
-							</a>
-						</h5>
-					</li>
-					<li>
-						<h5 className="devItem">
-							Kenny :<a href="https://phiri-kenny.netlify.app/" target="blank">
-								<img src={ken} className="logodev" alt="logo kenny" />
-							</a>
-						</h5>
-					</li>
-				</ul>
+				<p className="devItem">Christophe :</p>
+				<a href="https://cv-christophe-turpin.netlify.app/" target="blank">
+					<img src={chris} className="logodev" alt="logo christophe" />
+				</a>
+				<p className="devItem">Kenny :</p>
+				<a href="https://phiri-kenny.netlify.app/" target="blank">
+					<img src={ken} className="logodev" alt="logo kenny" />
+				</a>
 			</div>
 		</div>
 	);
